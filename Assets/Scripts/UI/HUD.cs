7731b53bac063188e05ad25780cd2097531af756
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-04-05 10:45:36 +0200

    HUD single

---
+++ Assets/Scripts/UI/HUD.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HUD : MonoBehaviour
+{
+    [SerializeField] private SingleHUD playerOneHUD;
+    [SerializeField] private SingleHUD playerTwoHUD;
+    [SerializeField] private SingleHUD playerThreeHUD;
+    [SerializeField] private SingleHUD playerFourHUD;
+
+
+}
---
+++ Assets/Scripts/UI/SingleHUD.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SingleHUD : MonoBehaviour
+{
+    [SerializeField] private Image rpmImage;
+    [SerializeField] private Image colorPlayerRPMImage;
+    [SerializeField] private Image characterPortraitImage;
+    [SerializeField] private Image characterImage;
+    [SerializeField] private TextMeshProUGUI rpmText;
+
+    private int maxRPM;
+    private const float MAX_FILL_AMOUNT = 0.75f;
+
+    private void SetPlayerHUD( Sprite chrarcterSprite , Color playerColor , int currentRPM , int maxRPM )
+    {
+        this.maxRPM = maxRPM;
+        rpmImage.fillAmount = MAX_FILL_AMOUNT * currentRPM / maxRPM;
+        colorPlayerRPMImage.color = playerColor;
+        characterPortraitImage.color = playerColor;
+        characterImage.sprite = chrarcterSprite;
+        rpmText.text = currentRPM.ToString();
+    }
+
+    private void UpdateHUD( int currentRPM )
+    {
+        rpmImage.fillAmount = MAX_FILL_AMOUNT * currentRPM / maxRPM;
+        rpmText.text = currentRPM.ToString();
+    }
+}
