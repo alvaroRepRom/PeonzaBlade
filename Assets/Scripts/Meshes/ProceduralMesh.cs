bd348a5ebbd4994e8daab2645df1bc908a2585ba
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-04-16 11:02:54 +0200

    Update ProceduralMesh.cs

--- Assets/Scripts/Meshes/ProceduralMesh.cs
+++ Assets/Scripts/Meshes/ProceduralMesh.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
@@ -6,7 +7,6 @@ public class ProceduralMesh : MonoBehaviour
     [SerializeField] private int xSize = 20;
     [SerializeField] private int zSize = 20;
     [SerializeField][Range(0.01f, 1)] private float cellSize = 0.5f;
-    [SerializeField] private bool createMesh;
     [SerializeField] private float radius = 15;
     [SerializeField] private float height = 6;
 
@@ -18,45 +18,20 @@ public class ProceduralMesh : MonoBehaviour
 
     private void Start()
     {
-        if ( createMesh )
-        {
-            CreateMesh();
-            UpdateMesh();
-        }
-        else
-        {
-            CloneMesh();
-        }
+        //CreateMesh();
+        //UpdateMesh();
 
-        MoveVertices();
-        UpdateMesh();
-        
+        //MoveVertices();
+        //UpdateMesh();
 
-        gameObject.AddComponent<MeshCollider>().sharedMesh = mesh;
-    }
+        //gameObject.AddComponent<MeshCollider>().sharedMesh = mesh;
 
-    private void CloneMesh()
-    {
-        MeshFilter meshFilter = GetComponent<MeshFilter>();
-        Mesh originalMesh     = meshFilter.sharedMesh;
-
-        mesh = new Mesh();
-        mesh.name       = "clone " + originalMesh.name;
-        mesh.vertices   = originalMesh.vertices;
-        mesh.triangles  = originalMesh.triangles;
-        mesh.normals    = originalMesh.normals;
-        mesh.uv         = originalMesh.uv;
-
-        meshFilter.mesh = mesh;
-
-        vertices  = mesh.vertices;
-        triangles = mesh.triangles;
+        StartCoroutine( MeshStep() );
     }
 
     private void MoveVertices()
     {
         vertices = mesh.vertices;
-        triangles = mesh.triangles;
 
         Vector3 sphereOrigin = new Vector3( 0 , height , 0 );
 
@@ -140,5 +115,63 @@ public class ProceduralMesh : MonoBehaviour
         mesh.RecalculateBounds();
         mesh.RecalculateTangents();
         mesh.RecalculateUVDistributionMetrics();
+    }
+
+
+    public  float multiplier = 0.5f;
+    private IEnumerator MeshStep()
+    {
+        CreateMesh();
+        UpdateMesh();
+
+        // get final vertices positions
+        Vector3[] finalVertices = mesh.vertices;
+
+        Vector3 sphereOrigin = new Vector3( 0 , height , 0 );
+
+        for ( int i = 0; i < mesh.vertexCount; i++ )
+        {
+            if ( Vector3.Distance( finalVertices[i] , sphereOrigin ) > radius )
+                continue;
+
+            float x = finalVertices[i].x;
+            float z = finalVertices[i].z;
+            float horizontal = Mathf.Sqrt( x * x + z * z );
+            float angle = Mathf.Atan2( height , horizontal );
+
+            float dhorizontal = Mathf.Cos( angle ) * radius - horizontal;
+            float dy = Mathf.Sin( angle ) * radius - height;
+
+            float horizontalAngle = Mathf.Atan2( z , x);
+            float dx = dhorizontal * Mathf.Cos( horizontalAngle );
+            float dz = dhorizontal * Mathf.Sin( horizontalAngle );
+
+            finalVertices[i] = new Vector3( x + dx , finalVertices[i].y - dy , z + dz );
+        }
+
+
+        Vector3[] initialVertices = mesh.vertices;
+        float elapsedTime = 0;
+
+        while ( elapsedTime < 1 )
+        {
+            elapsedTime += Time.deltaTime * multiplier;
+            if ( elapsedTime > 1 )
+                elapsedTime = 1;
+
+            for ( int i = 0; i < mesh.vertexCount; i++ )
+            {
+                float newX = Mathf.Lerp( initialVertices[i].x , finalVertices[i].x , elapsedTime );
+                float newY = Mathf.Lerp( initialVertices[i].y , finalVertices[i].y , elapsedTime );
+                float newZ = Mathf.Lerp( initialVertices[i].z , finalVertices[i].z , elapsedTime );
+
+                vertices[i] = new Vector3( newX , newY , newZ );
+            }
+
+            UpdateMesh();
+            yield return null;
+        }
+
+        gameObject.AddComponent<MeshCollider>().sharedMesh = mesh;
     }
 }
